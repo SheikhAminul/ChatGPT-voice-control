c258862e13340ca6e80e13617875894bb5f85934
Jpabon <jpabonb@bidda.net>
2024-05-03 18:38:58 -0500

    Add new url

--- src/utils/background.ts
+++ src/utils/background.ts
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener(async () => {
     // Reload tabs to apply content scripts
-    const tabs = await chrome.tabs.query({ url: ['https://chat.openai.com/*'] })
+    const tabs = await chrome.tabs.query({ url: ['https://chat.openai.com/*','https://chatgpt.com/*'] })
     tabs.forEach(({ id }) => chrome.tabs.reload(id))
 })
